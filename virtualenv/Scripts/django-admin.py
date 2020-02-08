87fe634f2f26b436618ef1ba63c42f62f23e5d08
Yashasvi Dadhe <yd1405@nyu.edu>
2020-02-08 14:39:04 +0530

    My first commit

---
+++ virtualenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ydadh\desktop\spring2020-cs-gy-9223-class\virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
